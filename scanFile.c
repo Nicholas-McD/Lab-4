8986db2767e1046c27f479525e85f38be38589e4
Nicholas-McD <Nicholasgmcdonough@outlook.com>
2026-03-06 04:37:04 +0000

    Add Lab2 assembly and printer source

--- scanFile.c
+++ scanFile.c
@@ -6,12 +6,15 @@ extern unsigned int sum;
 
 
 int main(){
-    int docSize = 50;
+
+    FILE *scan = fopen("data.txt", "r");
+
+    int docSize = 0;
+
+    fscanf(scan, "%d", &docSize);
     int docArr[docSize]; 
     char line[256]; 
     int index = 0;
-
-    FILE *scan = fopen("data.txt", "r");
 
     for (int i = 0; i < docSize; i++) {
         fscanf(scan, "%d", &docArr[i]); 
@@ -19,6 +22,8 @@ int main(){
 
     int total = sum_array(docArr, docSize); 
     printf("total sum = %d\n", total);
+    printf("size = %d\n", docSize);
+
 
     fclose(scan);
     return 0;
